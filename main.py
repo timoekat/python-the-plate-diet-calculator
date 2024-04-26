3b0b1ffaa6a48cb82a2b205928027330847ee0d0
timoekat <92270159+timoekat@users.noreply.github.com>
2024-04-26 21:37:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+fruits = []
+vegetables = []
+carbohydrates = []
+proteins = []
+
+fruits = input("Enter available fruits separated by space\n(for example: watermelon strawberry pineapple orange kiwi blueberry banana apple tangerine apricot)\n")
+fruits  = fruits.split()
+
+vegetables = input("Enter available vegetables separated by space\n(for example: carrot, tomato, salad, cucumber, zucchini, pepper)\n")
+vegetables  = vegetables.split()
+
+carbohydrates = input("Enter available carbohydrates separated by space\n(for example: rice, pasta, potato, bread, cereal, corn)\n")
+carbohydrates  = carbohydrates.split()
+
+proteins = input("Enter available proteins separated by space\n(for example: chicken, beef, fish, pork, shrimp)\n")
+proteins  = proteins.split()
+
+
+delimiter = ', '
+
+fruitstring = delimiter.join(fruits)
+vegetablestring = delimiter.join(vegetables)
+carbohydratesstring = delimiter.join(carbohydrates)
+proteinstring = delimiter.join(proteins)
+
+
+print('Half of your plate should contain: ' + str(fruitstring) + ' ,' + str(vegetablestring) + '\n25% of your plate should contain: ' + str(carbohydratesstring) + '\n25% of your plate should contain: '+ str(proteinstring))
+
